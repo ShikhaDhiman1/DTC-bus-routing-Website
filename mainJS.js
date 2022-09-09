f5337c724e9cd60780e8356b69885f8212c4c2fe
Shikha Dhiman <dshikha4403@gmail.com>
2022-09-09 09:05:20 +0530

    front end for DTC-Bus-Routing website

---
+++ mainJS.js
@@ -0,0 +1,18 @@
+// function menu(){
+//     var first = document.getElementById("bars").firstChild;
+//     var second = document.getElementById("bars").firstChild.nextSibling;
+//     document.write(first+"  |  "+second);
+// }
+// document.getElementsByClassName("btn").a.color="rgb(135, 178, 186)";
+function hoverIcon(){
+    document.getElementById("icon").class="fa fa-caret-up";
+}
+function btnHover(){
+     document.getElementsByClassName("btn").first.css.color="rgb(135, 178, 186)";
+}
+function menu(){
+    document.getElementsByClassName("div-h").innerHTML = "<h2 >Quick Links</h2><a href=''>Miscellaneous</a> | <a href=''>Copyright Policy</a> | <a href=''>Feedback</a> | <a href=''>Hyperlink Policy</a> | <a href=''>Privacy Policy</a> | <a href=''>Terms & Conditions</a> |<a href=''>Terms of Use</a> |  <a href=''>Security Policy</a><br>";
+    
+    
+    alert(str);
+}
